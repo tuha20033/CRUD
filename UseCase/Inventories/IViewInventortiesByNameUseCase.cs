37124a180b88784d61746ceb42712a64a73915fd
tu <hangoctu0206@gmail.com>
2026-03-10 16:11:21 +0700

    vcv

--- UseCase/Inventories/IViewInventortiesByNameUseCase.cs
+++
@@ -1,7 +0,0 @@
-﻿
-namespace UseCase.Inventories;
-
-public interface IViewInventortiesByNameUseCase
-{
-    Task<IEnumerable<CoreBusiness.Inventory>> ExecuteAsync(string name = "");
-}
--- UseCase/PluginInterface/IInventoryRepository.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace UseCase.PluginInterface;
-
-public interface IInventoryRepository
-{
-    Task<IEnumerable<CoreBusiness.Inventory>> GetInventoriesByNameAsync(string name);
-}
